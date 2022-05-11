dc2c3f6258fb490f8ecef6bf8d1fbfd559286171
BManduca <brunnomanducarfe@gmail.com>
2022-05-10 23:33:00 -0300

    atualizando repository Dia 2 - Semana 5 | 20220510

--- Aulas e exemplos/Semana #5/Dia2/desvendando_webpack/src/index.js
+++ Aulas e exemplos/Semana #5/Dia2/desvendando_webpack/src/index.js
@@ -1,4 +1,12 @@
-import moment from "moment/src/moment"
-import "planet.js"
+//nativo do JS acima da versao 6
+// import moment from "moment/src/moment"
 
-alert(moment().format("HH:mm:ss"))
+// alert(moment().format("HH:mm:ss"))
+
+function formatDate() { 
+    const date = new Date(); 
+    const formatDate = date.toLocaleString(); 
+    return formatDate; 
+}  
+
+alert(formatDate())
