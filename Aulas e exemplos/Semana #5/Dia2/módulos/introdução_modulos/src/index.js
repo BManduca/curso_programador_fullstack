dfbc9096190da8ccec0761bf093626c7c601aa7e
Brunno Manduca <brunnomanducarfe@gmail.com>
2022-05-11 10:37:00 -0300

    Delete Aulas e exemplos/Semana #5/Dia2/módulos directory

--- Aulas e exemplos/Semana #5/Dia2/módulos/introdução_modulos/src/index.js
+++
@@ -1,9 +0,0 @@
-function formatDate() { 
-    const date = new Date(); 
-    const formatDate = date.toLocaleString('br', { timeZone: 'UTC' }); 
-    return formatDate; 
-} 
-
-alert(formatDate())
-
-console.log('teste')
