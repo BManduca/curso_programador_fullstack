8e240c913d2efc01f73f1e43ec71ba9d76b6533c
BManduca <brunnomanducarfe@gmail.com>
2022-05-11 14:44:26 -0300

    Atualizando repository - aula de introdução aos módulos | 20220511

---
+++ Aulas e exemplos/Semana #5/Dia2/introducao_modulos/src/index.js
@@ -0,0 +1,12 @@
+//nativo do JS acima da versao 6
+// import moment from "moment/src/moment"
+
+// alert(moment().format("HH:mm:ss"))
+
+function formatDate() { 
+    const date = new Date(); 
+    const formatDate = date.toLocaleString('br', { timeZone: 'UTC' }); 
+    return formatDate; 
+} 
+
+alert(formatDate())
