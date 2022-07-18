3124234082b09e0f0c855976a4c0ff6d01136f70
BManduca <brunnomanduca@MacBook-Air-de-Brunno.local>
2022-07-17 23:20:01 -0300

    Iniciando dia2 da semana 6 | Criando projeto lorem ipsum

---
+++ Exercícios/Semana #6/criando_component_lorem_ipsum/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LoremIpsum from './components/Lorem_ipsum'
+import Helloworld from './components/Hello'
+
+function App() {
+  return (
+   <div>
+     <Helloworld/>
+     <LoremIpsum/>
+   </div>
+  );
+}
+
+export default App;
---
+++ Exercícios/Semana #6/criando_component_lorem_ipsum/src/components/Lorem_ipsum.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function LoremIpsum() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Cras tincidunt hendrerit enim semper luctus. 
+        Cras lacinia elementum libero, quis tristique lorem dictum vitae. 
+        Cras vestibulum leo in aliquet interdum. 
+        Nulla suscipit scelerisque lorem, at aliquam nulla pellentesque id. 
+        Donec at consectetur turpis.</p>
+    </div>
+  )
+}
+
+export default LoremIpsum;
